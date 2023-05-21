718b3b4f05e18c5664d008b3cc969ecd71b54906
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-05-21 09:03:23 +0530

    Create Bus Conductor.java

---
+++ Bus Conductor.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int findMoves(int n, int[] chairs, int[] passengers) {
+        // code here
+        Arrays.sort(chairs);
+        Arrays.sort(passengers);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=Math.abs(chairs[i]-passengers[i]);
+        }
+        return ans;
+    }
+}
