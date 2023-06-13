f12292896280523e774d3d6dfc695b46f2048c0d
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-06-13 19:17:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/k largest elements/k-largest-elements.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        // code here
+         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(arr[i]);
+        }
+
+        for (int i = k; i < n; i++) {
+            if (arr[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(arr[i]);
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+    }
+}
