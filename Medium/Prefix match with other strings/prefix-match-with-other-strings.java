3e696967e7e1ad2c0925f3149a3ed7d9ceea2bb9
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-06-24 13:27:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prefix match with other strings/prefix-match-with-other-strings.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            String[] arr = new String[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = sc.next();
+            }
+            
+            int k = Integer.parseInt(sc.next());
+            
+            String str = sc.next();
+            
+            Solution obj = new Solution();
+            int ans = obj.klengthpref(arr,n,k,str);
+            System.out.println(ans);
+            
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int klengthpref(String[] arr, int n, int k, String str)
+    {
+        if(k>str.length()){
+            return 0;
+        }
+        // code here
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].length()>=k){
+                if((str.substring(0,k)).equals(arr[i].substring(0,k))){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
