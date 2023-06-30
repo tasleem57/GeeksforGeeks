63fefff4cc795166d54bbf18e8689813eab7534c
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-06-30 20:28:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Is Binary Number Multiple of 3/is-binary-number-multiple-of-3.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out=new PrintWriter(System.out);
+            int t = Integer.parseInt(in.readLine().trim());
+            while(t-->0)
+                {
+                    String s = in.readLine().trim();
+                    Solution ob = new Solution();
+                    out.println(ob.isDivisible(s));
+                }
+                out.close();
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isDivisible(String s) {
+        // code here
+          int odd = 0;
+        int even = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(i % 2 == 0) {
+                even += (s.charAt(i) - '0');//evenSum
+            } else {
+                odd += (s.charAt(i) - '0');//odsSum
+            }
+        }
+        return  (even - odd) % 3 == 0 ? 1 : 0;
+    }
+}
