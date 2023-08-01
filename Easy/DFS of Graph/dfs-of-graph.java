3384465e83eba7068fa4a1e30ba3b66a981cb396
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-08-01 13:37:27 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/DFS of Graph/dfs-of-graph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+        Arrays.fill(vis, false);
+        
+        solve(ans, adj, vis, 0);
+        return ans;
+    }
+    
+    public void solve(ArrayList<Integer> ans, ArrayList<ArrayList<Integer>> adj, boolean[] vis, int i) {
+        vis[i] = true;
+        ans.add(i);
+            
+        ArrayList<Integer> nodes = adj.get(i);
+            
+        for(int j=0; j<nodes.size(); j++) {
+            if(!vis[nodes.get(j)]) {
+                solve(ans, adj, vis, nodes.get(j));
+            }
+        }
+    }
+}
